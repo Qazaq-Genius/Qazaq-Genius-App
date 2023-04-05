1a031a833c5c62c434ba1b49ff4d79029d226d80
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-04-05 18:32:58 +0200

    added .env variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,11 @@ import axios from 'axios';
 import Home from '../components/Home';
 import type { GetStaticProps } from 'next';
 
+const lyricsApi = process.env.LYRICS_API_HOST;
+
 // Fetch the song data from your API
 export const getStaticProps: GetStaticProps = async () => {
-  const { data: songData } = await axios.get('https://r0.fyi/qazaq-genius/50000001');
+  const { data: songData } = await axios.get(lyricsApi + '/song/50000001');
   return {
     props: {
       songData,
