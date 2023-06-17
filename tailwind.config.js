08010dee2fdd050ef3d22aa88716d44ceb19e900
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-06-17 18:58:06 +0200

    added custom colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+      },
+      colors: {
+        'highlight-green':        '#d7ff63',
+        'highlight-pink':         '#ffacd6',
+        'highlight-blue':         '#aedef8',
+        'highlight-yellow':       '#ffff4f',
+        'highlight-dark-yellow':  '#ffff0077',
+      }
+    }
   },
   variants: {
     extend: {},
