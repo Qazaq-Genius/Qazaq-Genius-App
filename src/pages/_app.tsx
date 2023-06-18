02f1f683259e5cd5443721c687a2512a70f0b16f
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-06-18 16:20:58 +0200

    added some og tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,12 @@ export default function App({ Component, pageProps }: AppProps) {
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <meta name="theme-color" content="#90B9D7" />
           <meta name="description" content="Qazaq Genius is a website that provides lyrics and translations of Kazakh songs." />
+
+          <meta property="og:type" content="website"/>
+          <meta property="og:url" content="https://https://qazaq-genius-app.vercel.app//"/>
+          <meta property="og:title" content="Qazaq Genius"/>
+          <meta property="og:description" content="Qazaq Genius is a website that provides lyrics and translations of Kazakh songs."/>
+          <meta property="og:image" content="https://repository-images.githubusercontent.com/609835063/bddcb5ea-6fcd-4b3f-852f-808042a7d50f"/>
         </Head>
     <Component {...pageProps} />
   </>
