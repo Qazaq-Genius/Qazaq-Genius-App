7d487123e29cfda85ac07809c457e8e54c9256bd
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-06-18 16:12:40 +0200

    added meta-tags and locale for SEO

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["kk","en", "ru"],
+    defaultLocale: "en",
+  },
   images: {
     remotePatterns: [
       {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../../styles/global.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+        <Head>
+          <title>Qazaq Genius</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="theme-color" content="#90B9D7" />
+          <meta name="description" content="Qazaq Genius is a website that provides lyrics and translations of Kazakh songs." />
+        </Head>
+    <Component {...pageProps} />
+  </>
 }
