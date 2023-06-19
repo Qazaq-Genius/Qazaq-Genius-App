0ee0aa9bade37cd8d2cd44a88d0a9b0a27fc10a7
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-06-19 20:21:09 +0200

    fallback true

--- src/pages/song/[id].tsx
+++ src/pages/song/[id].tsx
@@ -6,17 +6,18 @@ const lyricsApi = process.env.LYRICS_API_HOST;
 
 
 export async function getStaticPaths() {
-    const songs = (await axios.get(lyricsApi + '/songs')).data;
-    const paths = songs.map((id: any) => ({
-      params: {
-        id: id.toString(),
-      },
-    }));
-    return {
-      paths,
-      fallback: "blocking", // generate a new page if the user visits a page that doesn't exist
-    };
-  }
+  const songs = (await axios.get(lyricsApi + '/songs')).data;
+  const paths = songs.map((id: any) => ({
+    params: {
+      id: id.toString(),
+    },
+  }));
+  return {
+    paths,
+    // generate a new page if the user visits a page that doesn't exist
+    fallback: true,
+  };
+}
 
 export async function getStaticProps({ params }: any) {
   const { data: songData } = await axios.get(lyricsApi + '/song/' + params.id);
