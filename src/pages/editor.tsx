3ab3b21cd5607131909d58f9ec36822fdc258517
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-07-05 16:53:58 +0200

    more style adjustments

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -12,9 +12,9 @@ import { Verse, Artist } from '@/types';
 
 const Editor: React.FC = () => {
 
-	const [languages, setLanguages] = useState<string[]>([]);
+	const [languages, setLanguages] = useState<string[]>(['rus', 'eng', 'cyr']);
 	const [artists,   setArtists]   = useState<Artist[]>([]);
-	const [verse,     setVerse]     = useState<Verse[]>([]);
+	const [verse,     setVerse]     = useState<Verse[]>([{ lat: '', cyr: '', rus: '', eng: '' }]);
 	const [album,     setAlbum]     = useState<boolean>(false);
 
 	const setAlbumState = () => {
@@ -22,12 +22,10 @@ const Editor: React.FC = () => {
 	}
 
 	const addArtist = () => {
-		// add new Artist
 		setArtists([...artists, { lat: "", cyr: "" }]);
 	};
 
 	const addVerse = () => {
-		// add new verse
 		setVerse([...verse, { lat: '', cyr: '', rus: '', eng: '' }]);
 	};
 
@@ -103,49 +101,49 @@ const Editor: React.FC = () => {
 	return (
         <div  key={`site`}> {/*SiteContainer*/}
 			<div>
-				<div className="lg:mx-12.5% 2xl:mx-25%">
+				<div className="lg:mx-[19%] 2xl:mx-25% mx-1">
 					<div className="text-white flex justify-center text-5xl py-10">Add New Song</div>
 					<form method="POST" className="gap-3 flex-col flex" onSubmit={submitSong}>
 
 						{/* First block */}
-						<div className={`px-4 py-3 bg-white font-extralight text-lg`}>
+						<div className="sm:px-4 px-2 py-3 bg-white font-extralight flex flex-col text-lg gap-4">
 							{/* Language */}
-							<fieldset className="sm:flex-row flex-col sm:items-center flex my-5">
-								<div className="w-1/6">Languages</div>
+							<fieldset className="sm:flex-row flex-col sm:items-center flex">
+								<div className="w-1/6 min-w-[16.66666%]">Languages</div>
 								<div className="flex flex-row justify-evenly gap-2">
-									<CheckboxItem clickHandler={languageHandler} id="rus" text="Russian" />
-									<CheckboxItem clickHandler={languageHandler} id="eng" text="English" />
-									<CheckboxItem clickHandler={languageHandler} id="cyr" text="Qazaq Cyrillic" />
-									<CheckboxItem clickHandler={languageHandler} id="lat" text="Qazaq Latin" />
+									<CheckboxItem clickHandler={languageHandler} id="rus" text="Russian" className="h-16 sm:h-auto"/>
+									<CheckboxItem clickHandler={languageHandler} id="eng" text="English" className="h-16 sm:h-auto"/>
+									<CheckboxItem clickHandler={languageHandler} id="cyr" text="Qazaq Cyrillic" className="h-16 sm:h-auto"/>
+									<CheckboxItem clickHandler={languageHandler} id="lat" text="Qazaq Latin" className="h-16 sm:h-auto"/>
 								</div>
 							</fieldset>
 
 							{/* Title */}
-							<fieldset className="sm:flex-row flex-col sm:items-center flex my-5">
-								<div className="bg-white text-black rounded-md w-1/6">Title</div>
-								<div className="flex gap-2 w-5/6">
-									<TextBox name="title_cyr" placeholder="қазақша" className="" />
-									<TextBox name="title_lat" placeholder="qazaqşa" className="" />
-									<div className="w-0 md:w-4"></div>
+							<fieldset className="sm:flex-row flex-col sm:items-center flex">
+								<div className="text-black rounded-md min-w-[16.66666%]">Title</div>
+								<div className="flex flex-row gap-2 w-5/6">
+									<TextBox name="title_cyr" placeholder="қазақша" className="w-36 sm:w-auto" />
+									<TextBox name="title_lat" placeholder="qazaqşa" className="w-36 sm:w-auto" />
+									<div className="w-0 sm:min-w-4 sm:w-4"></div>
 								</div>
 							</fieldset>
 
 							{/* Artists */}
-							<fieldset className="sm:flex-row flex-col flex mt-5 mb-2 gap-1">
-								<div className="bg-white text-black rounded-md w-1/6">Artists</div>
-								<div className="flex-col flex mt-5 mb-2 gap-1">
+							<fieldset className="sm:flex-row flex-col sm:items-center flex mb-2">
+								<div className="text-black rounded-md min-w-[16.66666%]">Artists</div>
+								<div className="flex-col flex gap-1">
 									<div className="flex-row flex w-5/6">
 										<div className="flex gap-2">
-											<TextBox name="artist-cyr" placeholder="қазақша" className="" />
-											<TextBox name="artist-lat" placeholder="qazaqşa" className="" />
-											<div className="w-0 md:w-4"></div>
+											<TextBox name="artist-cyr" placeholder="қазақша" className="w-36 sm:w-auto" />
+											<TextBox name="artist-lat" placeholder="qazaqşa" className="w-36 sm:w-auto" />
+											<div className="w-0 sm:min-w-4 sm:w-4"></div>
 										</div>
 									</div>
 								{artists.map((value, index) => (
 									<div key={`artist-${index}`} className="flex-row flex  w-5/6">
 										<div className="flex">
-											<ArtistTextBox changeHandler={setArtistValue} index={index} language="cyr" value={value.cyr} name={`artist-${index}-cyr`} placeholder="қазақша" className=" mr-2" />
-											<ArtistTextBox changeHandler={setArtistValue} index={index} language="lat" value={value.lat} name={`artist-${index}-lat`} placeholder="qazaqşa" className=" mr-0 rounded-r-none border-r-0" />
+											<ArtistTextBox changeHandler={setArtistValue} index={index} language="cyr" value={value.cyr} name={`artist-${index}-cyr`} placeholder="қазақша" className="w-36 sm:w-auto mr-2" />
+											<ArtistTextBox changeHandler={setArtistValue} index={index} language="lat" value={value.lat} name={`artist-${index}-lat`} placeholder="qazaqşa" className="w-32 sm:w-auto mr-0 rounded-r-none border-r-0" />
 											<button id={`button-${index}`} onClick={() => removeArtist(index)} className="bg-red-500 hover:bg-red-700 text-white font-bold ml-0 h-10 px-3 rounded-r-md w-2 cursor-pointer" type="button">-</button>
 										</div>
 									</div>
@@ -158,32 +156,32 @@ const Editor: React.FC = () => {
 						</div>
 
 						{/* Second block */}
-						<div className={`px-4 py-3 bg-white font-extralight text-lg`}>
+						<div className="sm:px-4 px-2 py-3 bg-white font-extralight text-lg">
 						{/* Album */}
-							<fieldset className="">
-								<div className="sm:flex-row flex-col flex mb-2">
-									<div className="bg-white text-black rounded-md w-1/6">Album</div>
+							<fieldset className="flex flex-col gap-4">
+								<div className="sm:flex-row flex-col flex">
+									<div className="bg-white text-black rounded-md w-1/6 min-w-[16.66666%]">Album</div>
 									<div className="flex w-5/6 gap-2" >
-										<RadioButton id="album" text="Album" className="" checked={album === true} onChange={setAlbumState} />
-										<RadioButton id="no-album" text="No Album" className="" checked={album === false} onChange={setAlbumState} />
+										<RadioButton id="album" text="Album" className="w-44 sm:w-auto" checked={album === true} onChange={setAlbumState} />
+										<RadioButton id="no-album" text="No Album" className="w-44 sm:w-auto" checked={album === false} onChange={setAlbumState} />
 										<div className="w-0 md:w-4"></div>
 									</div>
 								</div>
 							{album &&
 								<div className="">
-									<div className="sm:flex-row flex-col flex mb-2">
-										<div className="bg-white text-black rounded-md w-1/6">Album Name</div>
+									<div className="sm:flex-row flex-col flex">
+										<div className="bg-white text-black rounded-md w-1/6 min-w-[16.66666%]">Album Name</div>
 										<div className="flex gap-2">
-											<TextBox name="album-cyr" placeholder="қазақша" className="" />
-											<TextBox name="album-lat" placeholder="qazaqşa" className="" />
+											<TextBox name="album-cyr" placeholder="қазақша" className="w-36 sm:w-auto" />
+											<TextBox name="album-lat" placeholder="qazaqşa" className="w-36 sm:w-auto" />
 											<div className="w-0 md:w-4"></div>
 										</div>
 									</div>
-									<div className="sm:flex-row flex-col flex mb-2">
-										<div className="bg-white text-black rounded-md w-1/6">Album Cover</div>
-										<div className="flex-row flex mt-2 w-5/6">
+									<div className="sm:flex-row flex-col flex">
+										<div className="bg-white text-black rounded-md w-1/6  min-w-[16.66666%]">Album Cover</div>
+										<div className="flex-row flex mt-2">
 											<input type="file" name="album-cover" id="album-cover" className="hidden w-0" />
-											<label htmlFor="album-cover" className="w-60 flex flex-row items-center gap-2 h-48 border-background border-2 rounded-md justify-center">
+											<label htmlFor="album-cover" className="w-72 h-72 sm:w-[31rem] sm:h-[31rem] flex flex-row items-center gap-2 border-background border-2 rounded-md justify-center">
 												<div className="text-gray-400">Drag and drop cover image</div>
 											</label>
 										</div>
@@ -194,10 +192,10 @@ const Editor: React.FC = () => {
 						</div>
 
 						{/* Third block */}
-						<div className={`px-4 py-3 bg-white font-extralight text-lg`}>
+						<div className={`sm:px-4 px-2 py-3 bg-white font-extralight text-lg`}>
 							{/* Verse */}
 							<fieldset className="gap-2 flex flex-col">
-								<legend className="bg-white text-black rounded-md px-2">Lyrics</legend>
+								<div className="bg-white text-black rounded-md px-2">Lyrics</div>
 							{verse.map((value, index) => (
 								<fieldset key={`line-${index}`} className=" bg-background flex flex-col rounded-md p-2 gap-2">
 								{languages.includes('cyr') && (
